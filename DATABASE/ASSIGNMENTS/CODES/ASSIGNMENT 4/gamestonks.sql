d90b8f07b51cc65f679a942f2a144db4eeed8507
Anushka <62488736+inushy@users.noreply.github.com>
2021-02-26 13:08:35 -0600

    initial commit

--- DATABASE/ASSIGNMENTS/CODES/ASSIGNMENT 4/gamestonks.sql
+++ DATABASE/ASSIGNMENTS/CODES/ASSIGNMENT 4/gamestonks.sql
@@ -9,108 +9,108 @@ drop TABLE IF EXISTS `Receiving`,`Holds`,`Buy/Sell`,`Revenues`,`Deals_With`,`Pro
 
 create table `Customer`
 (
-	`ID`				char(6)		 PRIMARY KEY -- Sets ID as the Primary Key for this table
+	`ID`				CHAR(6)		 PRIMARY KEY -- Sets ID as the Primary Key for this table
 );
 Create Table `Stock_Transactions`
 (
-	`Symbol`			char(6) 	 PRIMARY KEY, -- Sets SYMBOL as the Primary Key for this table	 
-    `Company_Name`		VARCHAR(30)  NOT NULL,
-    `Amount`			VARCHAR(30)	 nOT NULL
+	`Symbol`			CHAR(6) 	 PRIMARY KEY, -- Sets SYMBOL as the Primary Key for this table	 
+    	`Company_Name`			VARCHAR(30)  NOT NULL,
+   	`Amount`			VARCHAR(30)  NOT NULL
 );
 
 create table `Bank`
 (
-	`Account`			Varchar(30) PRIMARY KEY, -- Sets ACCOUNT as the Primary Key for this table
-    `Deposit`			INT(255)	NOT NULL,
-    `Withdraw`			int(255)	NOT NULL
+	`Account`			VARCHAR(30) PRIMARY KEY, -- Sets ACCOUNT as the Primary Key for this table
+    	`Deposit`			INT(255)	NOT NULL,
+    	`Withdraw`			INT(255)	NOT NULL
 );
 
 create table `Bank_Transactions`
 (
-	`Credentials`		Varchar(30) PRIMARY KEY, -- Sets CREDENTIALS as the Primary Key for this table
-    `Cancelled`			char(20)	NOT NULL,
-    `Approved`			char(20)	NOT NULL,
-  	`Pending`			char(20)	not NULL
+	`Credentials`			VARCHAR(30) PRIMARY KEY, -- Sets CREDENTIALS as the Primary Key for this table
+    	`Cancelled`			CHAR(20)	NOT NULL,
+    	`Approved`			CHAR(20)	NOT NULL,
+  	`Pending`			CHAR(20)	NOT NULL
 );
 
 create table `Customer_Account`
 (
-	`Account_Number`	varchar(30) PRIMARY KEY, -- Sets ACCOUNT NUMBER as the Primary Key for this table
-    `Routing_Number`	INT(20)		NOT NULL,
-    `ID`				char(6),
-  	 foreign key(ID) references Customer(ID)
+	`Account_Number`		VARCHAR(30) PRIMARY KEY, -- Sets ACCOUNT NUMBER as the Primary Key for this table
+    	`Routing_Number`		INT(20)		NOT NULL,
+    	`ID`				CHAR(6),
+  	 FOREIGN KEY(ID) REFERENCES Customer(ID)
 );
 
 create table `Order`
 (
-	`Order_ID`			Varchar(30) PRIMARY KEY, -- Sets ORDER ID as the Primary Key for this table
-    `Buying_Price`		INT(20)		NOT NULL,
-    `Selling_Price`		INT(20)		NOT NULL,
+	`Order_ID`			VARCHAR(30) PRIMARY KEY, -- Sets ORDER ID as the Primary Key for this table
+    	`Buying_Price`			INT(20)		NOT NULL,
+    	`Selling_Price`			INT(20)		NOT NULL,
   	`ID`				CHAR(6),
-  	 foreign key(ID) references Customer(ID)
+  	 FOREIGN KEY(ID) REFERENCES Customer(ID)
 );
 
 create table `Exchange`
 (
-	`Exchange_ID`		Varchar(30) PRIMARY KEY, -- Sets EXCHANGE ID as the Primary Key for this table
-  	`Account_Number`	varchar(30)	not NULL,
-  	 foreign key(`Account_Number`) references `Customer_Account`(`Account_Number`)
+	`Exchange_ID`			VARCHAR(30) PRIMARY KEY, -- Sets EXCHANGE ID as the Primary Key for this table
+  	`Account_Number`		VARCHAR(30)	NOT NULL,
+  	 FOREIGN KEY(`Account_Number`) REFERENCES `Customer_Account`(`Account_Number`)
 );
 
 create table `Proceeds`
 (
-	`Account_Number`	varchar(30), 
-	`Symbol`			char(6),  
+	`Account_Number`		VARCHAR(30), 
+	`Symbol`			CHAR(6),  
   	`Record`			VARCHAR(255),
-  	primary key(`Account_Number`, `Symbol`), -- Sets ACCOUNT NUMBER and SYMBOL as the Primary Key for this table
-  	Foreign key(`Account_Number`) REFERENCES `Customer_Account`(`Account_Number`),
-  	Foreign key(`Symbol`) REFERENCES `Stock_Transactions`(`Symbol`)
+  	PRIMARY KEY(`Account_Number`, `Symbol`), -- Sets ACCOUNT NUMBER and SYMBOL as the Primary Key for this table
+  	FOREIGN KEY(`Account_Number`) REFERENCES `Customer_Account`(`Account_Number`),
+  	FOREIGN KEY(`Symbol`) REFERENCES `Stock_Transactions`(`Symbol`)
 );
 
 create table `Deals_With`
 (
-	`Account_Number`	varchar(30),
-	`Account`			Varchar(30),	 
+	`Account_Number`		VARCHAR(30),
+	`Account`			VARCHAR(30),	 
   	`Record`			VARCHAR(255),
-  	primary key(`Account_Number`, `Account`), -- Sets ACCOUNT NUMBER and ACCOUNT as the Primary Key for this table
-  	Foreign key(`Account_Number`) REFERENCES `Customer_Account`(`Account_Number`),
-  	Foreign key(`Account`) REFERENCES `Bank`(`Account`)
+  	PRIMARY KEY(`Account_Number`, `Account`), -- Sets ACCOUNT NUMBER and ACCOUNT as the Primary Key for this table
+  	FOREIGN KEY(`Account_Number`) REFERENCES `Customer_Account`(`Account_Number`),
+  	FOREIGN KEY(`Account`) REFERENCES `Bank`(`Account`)
 );
 
 create table `Revenues`
 (
-	`Account`			varchar(30),
-	`Credentials`		Varchar(30),	 
+	`Account`			VARCHAR(30),
+	`Credentials`			VARCHAR(30),	 
   	`Record`			VARCHAR(255),
-  	primary key(`Account`, `Credentials`), -- Sets ACCOUNT and CREDENTIALS as the Primary Key for this table
-  	Foreign key(`Account`) REFERENCES `Bank`(`Account`),
-  	Foreign key(`Credentials`) REFERENCES `Bank_Transactions`(`Credentials`)
+  	PRIMARY KEY(`Account`, `Credentials`), -- Sets ACCOUNT and CREDENTIALS as the Primary Key for this table
+  	FOREIGN KEY(`Account`) REFERENCES `Bank`(`Account`),
+  	FOREIGN KEY(`Credentials`) REFERENCES `Bank_Transactions`(`Credentials`)
 );
 
 create table `Buy/Sell`
 (
-	`Order_ID`			varchar(30), 
+	`Order_ID`			VARCHAR(30), 
   	`Record`			VARCHAR(255),
-  	primary key(`Order_ID`), -- Sets Order ID as the Primary Key for this table
-  	Foreign key(`Order_ID`) REFERENCES `Order`(`Order_ID`)
+  	PRIMARY KEY(`Order_ID`), -- Sets Order ID as the Primary Key for this table
+  	FOREIGN KEY(`Order_ID`) REFERENCES `Order`(`Order_ID`)
 );
 
 create table `Holds`
 (
-	`Exchange_ID`		varchar(30),
-	`Symbol`			char(6),	 
+	`Exchange_ID`			VARCHAR(30),
+	`Symbol`			CHAR(6),	 
   	`Record`			VARCHAR(255),
-  	primary key(`Exchange_ID`, `Symbol`), -- Sets EXCHANGE ID and SYMBOL as the Primary Key for this table
-  	Foreign key(`Exchange_ID`) REFERENCES `Exchange`(`Exchange_ID`),
-  	Foreign key(`Symbol`) REFERENCES `Stock_Transactions`(`Symbol`)
+  	PRIMARY KEY(`Exchange_ID`, `Symbol`), -- Sets EXCHANGE ID and SYMBOL as the Primary Key for this table
+  	FOREIGN KEY(`Exchange_ID`) REFERENCES `Exchange`(`Exchange_ID`),
+  	FOREIGN KEY(`Symbol`) REFERENCES `Stock_Transactions`(`Symbol`)
 );
 
 create table `Receiving`
 (
-	`Order_ID`			varchar(30),
-	`Symbol`			char(6),	 
+	`Order_ID`			VARCHAR(30),
+	`Symbol`			CHAR(6),	 
   	`Record`			VARCHAR(255),
-  	primary key(`Order_ID`, `Symbol`), -- Sets ORDER ID and SYMBOL as the Primary Key for this table
-  	Foreign key(`Order_ID`) REFERENCES `Order`(`Order_ID`),
-  	Foreign key(`Symbol`) REFERENCES `Stock_Transactions`(`Symbol`)
+  	PRIMARY KEY(`Order_ID`, `Symbol`), -- Sets ORDER ID and SYMBOL as the Primary Key for this table
+  	FOREIGN KEY(`Order_ID`) REFERENCES `Order`(`Order_ID`),
+  	FOREIGN KEY(`Symbol`) REFERENCES `Stock_Transactions`(`Symbol`)
 );
